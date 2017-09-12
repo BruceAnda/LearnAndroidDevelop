530820258fd06a778b0ac4b81521713f8190f4c1
zhaoliang <2668645098@qq.com>
2017-09-12 15:37:13 +0800

    完成HelloWorld

--- app/src/main/java/cn/zhaoliang5156/learnandroiddevelop/ui/basic/corebasic/quickstart/demos/helloworld/HelloWorldActivity.kt
+++ app/src/main/java/cn/zhaoliang5156/learnandroiddevelop/ui/basic/corebasic/quickstart/demos/helloworld/HelloWorldActivity.kt
@@ -5,12 +5,17 @@ import cn.zhaoliang5156.learnandroiddevelop.R
 import cn.zhaoliang5156.learnandroiddevelop.ui.main.base.BaseActivity
 
 /**
- * HelloWorld
+ * 需求：在手机屏幕上显示HelloWorld的字样
  */
 class HelloWorldActivity : BaseActivity() {
 
+    /**
+     * 当Activity创建的时候有系统调用这个方法
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 设置在手机屏幕上显示的界面
         setContentView(R.layout.activity_hello_world)
 
         // 设置标题
